ac0e22d74a4eb162919a3c5b1fb399101c38c91e
Ishikawa <danay.bizhan@gmail.com>
2022-11-24 21:22:14 +0600

    1.31

--- src/main/java/group44/Project/controllers/LogController.java
+++ src/main/java/group44/Project/controllers/LogController.java
@@ -1,8 +1,10 @@
 package group44.Project.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class LogController {
     @GetMapping("/SignUp")
     public String SignUp(Model model) {
